d532773dc24e19782e57ac109804f67b2532e87f
Irvin <irvin@thishes.com>
2026-03-27 23:51:03 +0800

    修复exam.html路由语法错误

--- server/index.js
+++ server/index.js
@@ -50,7 +50,6 @@ app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
-n// exam.html 专用路由
 app.get('/exam.html', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist/exam.html'));
 });
